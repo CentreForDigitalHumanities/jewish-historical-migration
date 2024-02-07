142f64ee3676ab1dad438f2d0279726fe23f31c0
BeritJanssen <berit.janssen@gmail.com>
2024-02-07 10:27:27 +0100

    fix: use ReadOnlyModelViewSet

--- backend/data/views.py
+++ backend/data/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import permissions
 from .models import Record
 from .serializers import RecordSerializer
 
-class RecordViewSet(viewsets.ModelViewSet):
+
+class RecordViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows records to be viewed.
     """
     queryset = Record.objects.all()
     serializer_class = RecordSerializer
