f3d731f5ebf3c58c05905e47c58518dd7a4a9806
Tijmen Baarda <tijmenbaarda@gmail.com>
2025-10-31 11:27:17 +0100

    Upgrade to Angular 16

--- frontend/src/environments/version.ts
+++ frontend/src/environments/version.ts
@@ -1,3 +1,3 @@
 export const version = '0.3.0';
-export const buildTime = 'Fri Oct 31 2025 11:00:51 GMT+0100 (Midden-Europese standaardtijd)';
-export const sourceUrl = 'https://github.com/CentreForDigitalHumanities/jewish-historical-migration/tree/1218e099d92f55c5b98a7600cd0c38458a939afe';
+export const buildTime = 'Fri Oct 31 2025 11:26:47 GMT+0100 (Midden-Europese standaardtijd)';
+export const sourceUrl = 'https://github.com/CentreForDigitalHumanities/jewish-historical-migration/tree/3e744b928a8d21c7cb70ba43c662871cabb97cb9';
